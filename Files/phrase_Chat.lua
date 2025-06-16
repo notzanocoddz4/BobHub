2d82897b927a84df38e82834059ae23cf74d6e00
notzanocoddz4 <zanocoderversion@gmail.com>
2025-06-16 20:59:06 +0700

    initial commit

---
+++ Files/phrase_Chat.lua
@@ -0,0 +1,14 @@
+local Phrases_Chat = {
+    -- made by hqt and zano make pharases
+    "you are no match for my skill, go cry to your daddy about losing",
+    "you're useless and loud-bad combo.",
+    "bozo with a big mouth and no skills.",
+    "you are a bozo, just quit and go touch grass if you are so bad.",
+    "ez, you are so bad that you make me want to cry.",
+    "call admin for ban me, you are bozo",
+    "so ez, you are losing with advantages and youre gonna report me just because you cant beat me",
+    "you cant beat me even with good abilities, noob",
+    "you lost, stop crying.",
+}
+
+return Phrases_Chat
